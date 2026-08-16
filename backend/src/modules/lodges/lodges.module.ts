a1116010177061d5fe9e3e52b6df3c4559978f93
guestincome999-a11y <guestincome999@gmail.com>
2026-08-16 21:59:47 +0530

    feat: register lodge commission module services

--- backend/src/modules/lodges/lodges.module.ts
+++ backend/src/modules/lodges/lodges.module.ts
@@ -3,13 +3,15 @@ import { Module } from '@nestjs/common';
 import { RealtimeModule } from '../realtime/realtime.module';
 
 import { LodgeAccessService } from './lodge-access.service';
+import { LodgeCommissionController } from './commission.controller';
+import { LodgeCommissionService } from './commission.service';
 import { LodgesController } from './lodges.controller';
 import { LodgesService } from './lodges.service';
 
 @Module({
   imports: [RealtimeModule],
-  controllers: [LodgesController],
-  providers: [LodgesService, LodgeAccessService],
-  exports: [LodgesService, LodgeAccessService],
+  controllers: [LodgesController, LodgeCommissionController],
+  providers: [LodgesService, LodgeAccessService, LodgeCommissionService],
+  exports: [LodgesService, LodgeAccessService, LodgeCommissionService],
 })
 export class LodgesModule {}
