e9abd3ea1d937946291e36e72902e822da7ca214
guestincome999-a11y <guestincome999@gmail.com>
2026-08-18 22:15:47 +0530

    feat(payments): register payment controller

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { PaymentProvidersModule } from './providers/providers.module';
+import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
   imports: [PaymentProvidersModule],
+  controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService, PaymentProvidersModule],
 })
