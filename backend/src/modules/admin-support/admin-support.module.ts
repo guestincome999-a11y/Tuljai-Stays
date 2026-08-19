c89ffc452445b835461866d438a1101872f32e10
guestincome999-a11y <guestincome999@gmail.com>
2026-08-19 13:29:35 +0530

    feat: add admin customer support module

---
+++ backend/src/modules/admin-support/admin-support.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+
+import { AuditLogModule } from '../../shared/audit/audit-log.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { RealtimeModule } from '../realtime/realtime.module';
+
+import { AdminSupportController } from './admin-support.controller';
+import { AdminSupportService } from './admin-support.service';
+
+@Module({
+  imports: [AuditLogModule, NotificationsModule, RealtimeModule],
+  controllers: [AdminSupportController],
+  providers: [AdminSupportService],
+  exports: [AdminSupportService],
+})
+export class AdminSupportModule {}
