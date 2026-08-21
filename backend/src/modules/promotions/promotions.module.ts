2ce05b4c4e037f9e5426f45eb1f4cf2b4026e246
guestincome999-a11y <guestincome999@gmail.com>
2026-08-21 13:55:38 +0530

    feat: register promotions module

---
+++ backend/src/modules/promotions/promotions.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+import { AuditLogModule } from '../../shared/audit/audit-log.module';
+import { PromotionsController } from './promotions.controller';
+import { PromotionsService } from './promotions.service';
+
+@Module({
+  imports: [AuditLogModule],
+  controllers: [PromotionsController],
+  providers: [PromotionsService],
+  exports: [PromotionsService],
+})
+export class PromotionsModule {}
