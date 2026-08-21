1e3a8c0f1edf6ad1d09f0f99b04a1a244d6a467b
guestincome999-a11y <guestincome999@gmail.com>
2026-08-21 14:10:52 +0530

    fix: correct promotions module import groups

--- backend/src/modules/promotions/promotions.module.ts
+++ backend/src/modules/promotions/promotions.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { AuditLogModule } from '../../shared/audit/audit-log.module';
+
 import { PromotionsController } from './promotions.controller';
 import { PromotionsService } from './promotions.service';
 
-@Module({
-  imports: [AuditLogModule],
-  controllers: [PromotionsController],
-  providers: [PromotionsService],
-  exports: [PromotionsService],
-})
+@Module({ imports: [AuditLogModule], controllers: [PromotionsController], providers: [PromotionsService], exports: [PromotionsService] })
 export class PromotionsModule {}
