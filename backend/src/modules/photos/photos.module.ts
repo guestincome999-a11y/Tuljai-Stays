9fbdd9c2f0b87b5023f173a015d2ad0356cefbc5
guestincome999-a11y <guestincome999@gmail.com>
2026-09-01 08:33:23 +0530

    feat(photos): import StorageModule for lodge photo uploads

--- backend/src/modules/photos/photos.module.ts
+++ backend/src/modules/photos/photos.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { LodgesModule } from '../lodges/lodges.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { StorageModule } from '../storage/storage.module';
 
 import { PhotosController } from './photos.controller';
 import { PhotosService } from './photos.service';
 
 @Module({
-  imports: [LodgesModule, NotificationsModule],
+  imports: [LodgesModule, NotificationsModule, StorageModule],
   controllers: [PhotosController],
   providers: [PhotosService],
 })
